6ab8d23047c6f3cc49a021089f6a7e7272ea9e9c
Paulo Pacheco <paulo95.emp@gmail.com>
2020-09-18 14:52:33 -0300

    Add Auth context

---
+++ src/hooks/AuthContext.tsx
@@ -0,0 +1,71 @@
+import React, { createContext, useCallback, useState, useContext } from 'react';
+import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: Record<string, unknown>;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface IAuthContext {
+  user: AuthState['user'];
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+const AuthContext = createContext<IAuthContext>({} as IAuthContext);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) return { token, user: JSON.parse(user) };
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    try {
+      const { data: signInData } = await api.post('sessions', {
+        email,
+        password,
+      });
+
+      const { token, userWithouPassword: user } = signInData;
+
+      localStorage.setItem('@GoBarber:token', token);
+      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+      setData({ token, user });
+    } catch ({ response }) {
+      const { data: error } = response;
+      console.log(error);
+    }
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuth(): IAuthContext {
+  const context = useContext(AuthContext);
+
+  if (!context)
+    throw new Error('useAuth can only be used within an AuthProvider');
+
+  return context;
+}
